d369ba89a6f6f3135d14bd57244327b796c3303e
Joel Stephen <joel.stephen56.sj@gmail.com>
2024-10-13 18:24:13 +0100

    feat/created task app and implemented the models.py

---
+++ tasks/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    LOW = 'Low'
+    MEDIUM = 'Medium'
+    HIGH = 'High'
+
+    PRIORITY_CHOICES = [
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        (HIGH, 'High'),
+    ]
+
+    PENDING = 'Pending'
+    COMPLETED = 'Completed'
+
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (COMPLETED, 'Completed'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateTimeField()
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default=MEDIUM)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=PENDING)
+    completed_at = models.DateTimeField(null=True, blank=True)  # To store the timestamp when the task is completed
+
+    # Relationships
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.title
